c752344a53365dcb5a279f569d0ff8e6ed7419af
5-pebbles <5-pebble@protonmail.com>
2024-08-16 19:08:40 -0400

    fix(visualizer): clean up;

--- visualizer/src/main.rs
+++ visualizer/src/main.rs
@@ -1,43 +1,33 @@
 use std::{
+    fs::File,
     io::{self, Read, Write},
     thread,
     time::Duration,
 };
-use termion::{
-    cursor::{BlinkingBlock, SteadyBlock},
-    get_tty,
-    raw::IntoRawMode,
-};
+
+const BLINKING_UNDERLINE: &str = "\x1b[3 q";
+const STEADY_UNDERLINE: &str = "\x1b[4 q";
 
 fn main() -> io::Result<()> {
     let mut content = String::new();
     io::stdin().read_to_string(&mut content)?;
 
-    let sentences: Vec<&str> = content
-        .split_inclusive(&['.', '!', '?', ';', '\n'])
-        .collect();
+    let mut tty = File::open("/dev/tty")?;
+    print!("{STEADY_UNDERLINE}");
+    io::stdout().flush()?;
 
-    let mut stdout = io::stdout().lock();
-
-    for sentence in sentences {
-        write!(stdout, "{SteadyBlock}")?;
-        for c in sentence.chars() {
-            write!(stdout, "{}", c)?;
-            stdout.flush()?;
-
-            let wait_duration = Duration::from_millis(45);
-            thread::sleep(wait_duration);
+    for c in content.chars() {
+        if c == '\n' {
+            print!("{BLINKING_UNDERLINE}");
+            io::stdout().flush()?;
+            tty.read_exact(&mut [0])?;
+            print!("{STEADY_UNDERLINE}");
+            io::stdout().flush()?;
+            continue;
         }
-        write!(stdout, "{BlinkingBlock}")?;
-
-        // Wait for a keypress
-        let mut tty = get_tty()?.into_raw_mode()?;
-
-        let mut buf = [0; 1];
-        tty.read_exact(&mut buf)?;
-        if buf[0] == b'q' {
-            break;
-        }
+        print!("{}", c);
+        io::stdout().flush()?;
+        thread::sleep(Duration::from_millis(40));
     }
 
     Ok(())
